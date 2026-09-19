fff02ddfce4b8c90fefa6b919384eb78f62c526e
Agustin Xocua Dimayuga <Axocuadi@calpoly.edu>
2026-09-19 12:37:31 -0700

    Fixed backend bug

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,9 +1,11 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -84,14 +86,14 @@ app.get("/users/:id", (req, res) => {
 });
 
 const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
+  const userToAdd = { ...user, id: crypto.randomUUID() };
+  users["users_list"].push(userToAdd);
+  return userToAdd;
 };
 
 app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  addUser(userToAdd);
-  res.send();
+  const addedUser = addUser(req.body);
+  res.status(201).send(addedUser);
 });
 
 const deleteUser = (id) => {
